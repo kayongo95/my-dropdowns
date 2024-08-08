cde7ab112cb2bb2b155ba1dd67be12a14b1c7c38
Kayongo Nicholas <nicklasoncrystal@gmail.com>
2024-08-08 16:37:45 +0300

    drop-menu
    my drop-menu

---
+++ dropdown.js
@@ -0,0 +1,10 @@
+function myMenu() {
+   var menu = document.getElementById('drop-menu');
+   
+   if (menu.style.display === 'block') {
+     menu.style.display = 'none';
+   } else {
+     menu.style.display = 'block';
+}
+}
+
